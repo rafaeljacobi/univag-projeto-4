7e6ba4d2fc125d728bcf24e97abbb920fcf9a30e
rafaeljacobi <82853878+rafaeljacobi@users.noreply.github.com>
2024-07-01 06:54:34 -0400

    Update paciente.py
    Acrescentado campo CPF

--- models/paciente.py
+++ models/paciente.py
@@ -7,21 +7,23 @@ class Paciente(db.Model):
     email = db.Column(db.String(255))
     telefone = db.Column(db.String(255))
     sexo = db.Column(db.String(255))
+    cpf = db.Column(db.String(14))
     
-    def __init__(self, nome, data_nascimento, email, telefone, sexo):
+    def __init__(self, nome, data_nascimento, email, telefone, sexo, cpf):
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.email = email
         self.telefone = telefone
         self.sexo = sexo
+        self.cpf = cpf
     
     def _repr_(self):
-        return f"Paciente(id: {self.id}, nome: {self.nome}, data_nascimento: {self.data_nascimento}, email: {self.data_nascimento}, telefone: {self.telefone}, sexo: {self.sexo})"
+        return f"Paciente(id: {self.id}, nome: {self.nome}, data_nascimento: {self.data_nascimento}, email: {self.data_nascimento}, telefone: {self.telefone}, sexo: {self.sexo}, cpf: {self.cpf})"
  
 # Cria um novo paciente
-def criar_paciente(nome, data_nascimento, email, telefone, sexo):
+def criar_paciente(nome, data_nascimento, email, telefone, sexo, cpf):
     crud_paciente = CRUD(Paciente)
-    crud_paciente.create({"nome" : nome, "data_nascimento" : data_nascimento, "email" : email, "telefone" : telefone, "sexo" : sexo})
+    crud_paciente.create({"nome" : nome, "data_nascimento" : data_nascimento, "email" : email, "telefone" : telefone, "sexo" : sexo, "cpf": cpf})
    
 # Retona o paciente relacionado a 'atributo' = 'valor' (ex. 'id' = 1, 'nome' = 'Rafael')
 def obter_paciente(atributo, valor):
@@ -30,9 +32,9 @@ def obter_paciente(atributo, valor):
     return estacao
 
 # Atualiza o paciente  relacionado a 'id'
-def atualizar_paciente(id, nome, data_nascimento, email, telefone, sexo):
+def atualizar_paciente(id, nome, data_nascimento, email, telefone, sexo, cpf):
     crud_paciente = CRUD(Paciente)
-    crud_paciente.update(id, {"nome" : nome, "data_nascimento" : data_nascimento, "email" : email, "telefone" : telefone, "sexo" : sexo})
+    crud_paciente.update(id, {"nome" : nome, "data_nascimento" : data_nascimento, "email" : email, "telefone" : telefone, "sexo" : sexo, "cpf": cpf})
 
 # Excluir o paciente relacioando ao 'id'
 def excluir_paciente(id):
@@ -42,6 +44,6 @@ def excluir_paciente(id):
 # Retorna todos os pacientes
 def obter_todos_pacientes():
     crud_paciente = CRUD(Paciente)
-    estacoes = crud_paciente.get_all()
-    return estacoes
+    pacientes = crud_paciente.get_all()
+    return pacientes
     
