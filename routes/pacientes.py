9a04e95f052a698bc5880a6582b840df2186c5e6
rafaeljacobi <82853878+rafaeljacobi@users.noreply.github.com>
2024-07-01 06:55:24 -0400

    Update pacientes.py
    Atualizado campo CPF

--- routes/pacientes.py
+++ routes/pacientes.py
@@ -22,6 +22,7 @@ def criar():
     if request.method == "POST":       
         form_data = request.form.to_dict()
         
+        cpf = form_data.get("cpf")
         nome = form_data.get("nome")
         data_nascimento_str = form_data.get("data_nascimento")
         data_nascimento = datetime.datetime.strptime(data_nascimento_str, "%Y-%m-%d")
@@ -34,7 +35,7 @@ def criar():
             flash("Paciente já cadastrado!", "error")
             return render_template("paciente/criar.html")
         
-        criar_paciente(nome, data_nascimento, email, telefone, sexo)
+        criar_paciente(nome, data_nascimento, email, telefone, sexo, cpf)
     
         flash("Paciente criado com suscesso!", "success")
         return redirect(url_for("pacientes.pacientes"))
@@ -43,9 +44,9 @@ def criar():
     return render_template("paciente/criar.html") 
 
 # Cria um novo paciente via recepcao
-@paciente_bp.route("/paciente/recepcao_criar/<int:setor_id>/<int:estacao_id>/<int:senha_atual_id>", methods=["GET", "POST"])
+@paciente_bp.route("/paciente/recepcao_criar/<int:setor_id>/<int:estacao_id>/<int:senha_atual_id>/<string:cpf>", methods=["GET", "POST"])
 @login_required
-def recepcao_criar(setor_id, estacao_id, senha_atual_id):
+def recepcao_criar(setor_id, estacao_id, senha_atual_id, cpf):
     senha = obter_senha("id", senha_atual_id)
     
     if request.method == "POST":
@@ -53,6 +54,7 @@ def recepcao_criar(setor_id, estacao_id, senha_atual_id):
         
         form_data = request.form.to_dict()
         
+        cpf = form_data.get("cpf")
         nome = form_data.get("nome")
         data_nascimento_str = form_data.get("data_nascimento")
         data_nascimento = datetime.datetime.strptime(data_nascimento_str, "%Y-%m-%d")
@@ -63,38 +65,69 @@ def recepcao_criar(setor_id, estacao_id, senha_atual_id):
         paciente = obter_paciente("nome", nome)
         if paciente:
             flash("Paciente já cadastrado!", "error")
-            return render_template("paciente/criar.html", setor_id, estacao_id, senha_atual_id)
+            return render_template("paciente/criar.html", setor_id, estacao_id, senha_atual_id, cpf)
         
-        criar_paciente(nome, data_nascimento, email, telefone, sexo)
+        criar_paciente(nome, data_nascimento, email, telefone, sexo, cpf)
     
         flash("Paciente criado com suscesso!", "success")
         return redirect(url_for("recepcao.senhas", setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id, acao = "encerrar_atendimento"))
     
     #request.method == "GET":
-    return render_template("paciente/criar.html") 
+    return render_template("paciente/criar.html", setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id, cpf = cpf) 
 
 # Edita o paciente relacionado ao 'paciente_id'
 @paciente_bp.route("/paciente/editar/<int:paciente_id>", methods=["GET", "POST"])
 @login_required
 def editar(paciente_id):
     paciente = obter_paciente("id", paciente_id)
-        
+    data_nascimento = paciente.data_nascimento.strftime("%Y-%m-%d")
+
     if request.method == "POST":
         form_data = request.form.to_dict()
         
+        cpf = form_data.get("cpf")
         nome = form_data.get("nome")
-        data_nascimento = form_data.get("data_nascimento")
+        data_nascimento_str = form_data.get("data_nascimento")
+        data_nascimento = datetime.datetime.strptime(data_nascimento_str, "%Y-%m-%d")
         email = form_data.get("email")
         telefone = form_data.get("telefone")
         sexo = form_data.get("sexo")
 
-        atualizar_paciente(paciente.id, nome, data_nascimento, email, telefone, sexo)
+        atualizar_paciente(paciente.id, nome, data_nascimento, email, telefone, sexo, cpf)
         
-        flash("Paciente editador com suscesso!", "success")
+        flash("Paciente atualizado com suscesso!", "success")
         return redirect(url_for("pacientes.pacientes"))
         
     # request.method == "GET":
-    return render_template("paciente/editar.html", perfil = paciente)
+    return render_template("paciente/editar.html", paciente = paciente, data_nascimento = data_nascimento)
+
+
+
+# Edita o paciente relacionado ao 'paciente_id' via recepção
+@paciente_bp.route("/paciente/recepcao_editar/<int:paciente_id>/<int:setor_id>/<int:estacao_id>/<int:senha_atual_id>", methods=["GET", "POST"])
+@login_required
+def recepcao_editar(paciente_id, setor_id, estacao_id, senha_atual_id):
+    paciente = obter_paciente("id", paciente_id)
+    data_nascimento = paciente.data_nascimento.strftime("%Y-%m-%d")
+        
+    if request.method == "POST":
+        form_data = request.form.to_dict()
+        
+        cpf = form_data.get("cpf")
+        nome = form_data.get("nome")
+        data_nascimento_str = form_data.get("data_nascimento")
+        data_nascimento = datetime.datetime.strptime(data_nascimento_str, "%Y-%m-%d")
+        email = form_data.get("email")
+        telefone = form_data.get("telefone")
+        sexo = form_data.get("sexo")
+
+        atualizar_paciente(paciente.id, nome, data_nascimento, email, telefone, sexo, cpf)
+        
+        flash("Paciente atualizado com suscesso!", "success")
+        return redirect(url_for("recepcao.senhas", setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id, acao = 'encerrar_atendimento'))
+        
+    # request.method == "GET":
+    return render_template("paciente/editar.html", paciente = paciente, data_nascimento = data_nascimento, setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id)
 
 # Exclui o paciente relacionado a 'paciente_id'
 @paciente_bp.route("/paciente/excluir/<int:paciente_id>", methods=["GET", "POST"])
@@ -110,3 +143,25 @@ def excluir(paciente_id):
     
     # request.method == "GET":
     return render_template("paciente/excluir-confirmacao.html", paciente = paciente)
+
+# Pesquisa o paciente relacionado a 'cpf'
+@paciente_bp.route("/paciente/pesquisar/<int:setor_id>/<int:estacao_id>/<int:senha_atual_id>", methods=["GET", "POST"])
+@login_required
+def pesquisar(setor_id, estacao_id, senha_atual_id): 
+   
+    if request.method == "POST":
+        form_data = request.form.to_dict()
+        
+        cpf = form_data.get("cpf")
+        #setor_id = form_data.get("setor_id")
+        #estacao_id = form_data.get("estacao_id")
+        #senha_atual_id = form_data.get("senha_atual_id")
+        
+        paciente = obter_paciente("cpf", cpf)
+        if paciente:
+            return redirect(url_for("pacientes.recepcao_editar", paciente_id = paciente.id, setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id, cpf = paciente.cpf))
+        else:
+            return redirect(url_for("pacientes.recepcao_criar", setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id, cpf = cpf))
+            
+    # request.method == "GET":
+    return render_template("paciente/pesquisar.html", setor_id = setor_id, estacao_id = estacao_id, senha_atual_id = senha_atual_id)
